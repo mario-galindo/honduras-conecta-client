28b8aaadb0b7575c0fa00362ed3e0c1175edc351
Mario-Galindo <mgalindo9410@gmail.com>
2020-05-08 23:29:49 -0600

    add components for auth system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,22 @@ import { environment } from '../environments/environment'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ForgotpasswordComponent } from './components/forgotpassword/forgotpassword.component';
+import { VerifyemailComponent } from './components/verifyemail/verifyemail.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SigninComponent,
+    SignupComponent,
+    ForgotpasswordComponent,
+    VerifyemailComponent
   ],
   imports: [
     BrowserModule,
