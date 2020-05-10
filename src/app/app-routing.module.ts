78e24713bf264c48e658ec8c758bf287e549afb7
Mario-Galindo <mgalindo9410@gmail.com>
2020-05-09 21:13:59 -0600

    add login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SuperSecretComponent } from './super-secret/super-secret.component';
 import { AuthGuard } from './services/auth.guard'
+import { LoginComponent } from './components/login/login.component';
 
 
 const routes: Routes = [
-  { path: 'secret', component: SuperSecretComponent, canActivate: [AuthGuard] }
+  { path: 'secret', component: SuperSecretComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
