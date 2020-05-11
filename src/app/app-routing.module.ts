ec7c40ef69a1e52276eb9fd679c8067c2fb7aac6
Mario-Galindo <mgalindo9410@gmail.com>
2020-05-10 22:36:18 -0600

    add email login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { SuperSecretComponent } from './super-secret/super-secret.component';
 import { AuthGuard } from './services/auth.guard'
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { EmailComponent } from './components/email/email.component';
 
 
 const routes: Routes = [
   { path: 'secret', component: SuperSecretComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent }
+  { path: 'signup', component: SignupComponent },
+  { path: 'login-email', component: EmailComponent }
 ];
 
 @NgModule({
--- src/app/components/email/email.component.ts
+++ src/app/components/email/email.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
+import { moveIn, fallIn } from '../../router.animations'
 
 @Component({
   selector: 'app-email',
   templateUrl: './email.component.html',
-  styleUrls: ['./email.component.css']
+  styleUrls: ['./email.component.css'],
+  animations: [moveIn(), fallIn()],
+  host: { '[@moveIn]': '' }
 })
 export class EmailComponent implements OnInit {
 
-  constructor() { }
+  state: string = '';
+  error: any;
+
+  constructor(private router:Router) { }
+
+  onSubmit(formData) {
+    
+  }
 
   ngOnInit() {
   }
