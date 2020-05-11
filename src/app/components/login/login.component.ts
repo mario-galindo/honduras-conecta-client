0fa453fae5528b6b19b63c47e1d72ffc00a09212
Mario-Galindo <mgalindo9410@gmail.com>
2020-05-11 13:32:26 -0600

    remove animation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit,HostBinding } from '@angular/core';
 import { Router } from '@angular/router';
-import {moveIn} from '../../router.animations'
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
-  animations: [moveIn()],
-  host: {'[@moveIn]': ''}
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
